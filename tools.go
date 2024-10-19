937d292dbb22a107b0ac3290d4446e51d9679f3d
ChrisShia <christos.shiakas@gmail.com>
2024-10-19 19:30:00 +0300

    Added offset method to ModCounter and tests.

--- tools.go
+++ tools.go
@@ -4,6 +4,12 @@ type ModuloCounter struct {
 	start, modulo, i, c int
 }
 
+func NewModCounterWithOffset(start, modulo, offset int) *ModuloCounter {
+	c := &ModuloCounter{start: start, modulo: modulo}
+	c.Offset(offset)
+	return c
+}
+
 func NewModCounter(start, mod int) *ModuloCounter {
 	return &ModuloCounter{start: start, modulo: mod}
 }
@@ -16,12 +22,12 @@ func (mc *ModuloCounter) ModIndex() int {
 	return mc.c + mc.start
 }
 
+func (mc *ModuloCounter) Offset(offset int) {
+	mc.i += offset
+}
+
 func (mc *ModuloCounter) Next() int {
 	mc.i++
-	if mc.i < mc.modulo {
-		mc.c = mc.i
-		return mc.ModIndex()
-	}
 	mc.c = mc.i % mc.modulo
 	return mc.ModIndex()
 }
--- tools_test.go
+++ tools_test.go
@@ -50,3 +50,29 @@ func Test_CounterModulo(t *testing.T) {
 		})
 	}
 }
+
+func Test_CounterModuloWithOffset(t *testing.T) {
+	tests := []struct {
+		name     string
+		advance  int
+		start    int
+		modulo   int
+		offset   int
+		expected int
+	}{
+		{"Start from -2 offset 2 advance 5 times mod 3", 5, -2, 3, 2, -1},
+		{"Start from 100 offset 4 advance 5 times mod 3", 5, 100, 3, 4, 100},
+	}
+	for _, tt := range tests {
+		counter := NewModCounterWithOffset(tt.start, tt.modulo, tt.offset)
+		t.Run(tt.name, func(t *testing.T) {
+			for i := 1; i <= tt.advance; i++ {
+				counter.Next()
+			}
+			ans := counter.ModIndex()
+			if ans != tt.expected {
+				t.Errorf("Actual = %d, expected %d", ans, tt.expected)
+			}
+		})
+	}
+}
